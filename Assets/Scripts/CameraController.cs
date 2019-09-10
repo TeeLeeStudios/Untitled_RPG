fa54ded24f7d7ecf6df6df87ce4c67834e625002
Thomas <teeleestudios@gmail.com>
2019-09-09 23:44:01 -0700

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CameraController : MonoBehaviour
+{
+    public float FieldOfView = 70f;
+    public float sensitivity = 0.5f;
+    public float smoothing = 4.0f;
+    // the character is the capsule
+    public GameObject character;
+    // get the incremental value of mouse moving
+    private Vector2 mouseLook;
+    // smooth the mouse moving
+    private Vector2 smoothV;
+    private Camera myCamera;
+
+    void Start()
+    {
+        if (!this.transform.parent)
+        {
+            gameObject.GetComponent<Camera>().enabled = false;
+            gameObject.GetComponent<AudioListener>().enabled = false;
+            return;
+        }
+        if (!this.transform.parent.GetComponent<Controls>().isLocalPlayer)
+        {
+            gameObject.GetComponent<Camera>().enabled = false;
+            gameObject.GetComponent<AudioListener>().enabled = false;
+            return;
+        }
+        character = this.transform.parent.gameObject;
+        //Setting FOV
+        myCamera = GetComponent<Camera>();
+        if (myCamera)
+        {
+            myCamera.fieldOfView = FieldOfView;
+        }
+    }
+
+    void Update()
+    {
+        if (!this.transform.parent)
+        {
+            return;
+        }
+        if (!this.transform.parent.GetComponent<Controls>().isLocalPlayer)
+        {
+            return;
+        }
+
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        // the interpolated float result between the two float values
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        // incrementally add to the camera look
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90, 90);
+        // vector3.right means the x-axis
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+        
+    }
+
+}
---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Controls : NetworkBehaviour
+{
+
+    private Rigidbody rb;
+    private new Transform transform;
+    private Vector3 Direction;
+    public int Speed;
+    public float JumpHeight;
+    private float zAxis;
+    private float xAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked;
+        if (JumpHeight == 0) JumpHeight = 8;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(!isLocalPlayer)
+        {
+
+            return;
+        }
+        
+        //X and Z movement
+        zAxis = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        xAxis = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        transform.Translate(xAxis, 0, zAxis);
+
+        //Y movement
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (_isGrounded())
+            {
+                rb.AddForce(0, JumpHeight, 0, ForceMode.Impulse);
+                Debug.Log("Jumping");
+            }
+        }
+
+        //Toggle the Cursor lock
+        if (Input.GetButtonDown("CursorLock")) ToggleCursorLock();
+
+    }
+
+    void ToggleCursorLock()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Debug.Log("Cursor is now unlocked");
+            return;
+            
+        }
+        
+        if (Cursor.lockState == CursorLockMode.None)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            Debug.Log("Cursor is now locked");
+            return;
+        }
+    }
+
+    
+    bool _isGrounded()
+    {
+        //bit shift the index layer (9) to get a bit mask
+        int LayerMask = 1 << 9;
+
+        //Create a new Ray
+        Ray ray = new Ray(transform.position, transform.up * -1);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit, transform.localScale.y + 0.2f, LayerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down)*1 , Color.black);
+            Debug.Log("Hit: " + hit);
+            return true;
+        }
+
+        Debug.Log("Nothing was hit");
+        return false;
+    }
+
+}
