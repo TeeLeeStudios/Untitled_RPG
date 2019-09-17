8b3588e975e351d34b9458a9fd12d6958fe2ef37
Thomas <teeleestudios@gmail.com>
2019-09-17 16:35:10 -0700

    Added state manager to Player class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,108 +7,87 @@ public class Player : MonoBehaviour
     //Singleton instance of this class
     public Player player;
 
-    private readonly string[] AcceptableStates = {"idle","sitting","walking","running","swimming","mounted","unconcious" };
+    private readonly string[] AcceptableStates = {"idle","sitting","laying","prone","walking","running","swimming","mounted","unconcious","revival","dead" };
+
 
     void Awake()
     {
         player = this;
+        MaxHP = 100;
+        CurrentHP = 100;
+        MaxMP = 100;
+        CurrentMP = 100;
+        MaxStamina = 100;
+        CurrentStamina = 100;
+        Hunger = 100;
+        Thirst = 100;
+        State = "idle";
     }
 
+    #region Properties
     int CurrentHP
     {
         get
-        {
-            return CurrentHP;
-        }
+        { return CurrentHP; }
 
         set
-        {
-            CurrentHP = value;
-        }
+        { CurrentHP = value; }
     }
 
-    int MaxHP
+    public int MaxHP
     {
         get
-        {
-            return MaxHP;
-        }
+        { return MaxHP; }
         set
-        {
-            MaxHP = value;
-        }
+        { MaxHP = value; }
     }
 
     int CurrentMP
     {
         get
-        {
-            return CurrentMP;
-        }
+        { return CurrentMP; }
         set
-        {
-            CurrentMP = value;
-        }
+        { CurrentMP = value; }
     }
 
     int MaxMP
     {
         get
-        {
-            return MaxMP;
-        }
+        { return MaxMP; }
         set
-        {
-            MaxMP = value;
-        }
+        { MaxMP = value; }
     }
     
     int CurrentStamina
     {
         get
-        {
-            return CurrentStamina;
-        }
+        { return CurrentStamina; }
         set
-        {
-            CurrentStamina = value;
-        }
+        { CurrentStamina = value; }
     }
 
     int MaxStamina
     {
         get
-        {
-            return MaxStamina;
-        }
+        { return MaxStamina; }
         set
-        {
-            MaxStamina = value;
-        }
+        { MaxStamina = value; }
     }
 
     int Hunger
     {
         get
-        {
-            return Hunger;
-        }
+        { return Hunger; }
         set
-        {
-            Hunger = value;
-        }
+        { Hunger = value; }
     }
 
     int Thirst
     {
         get
-        {
-            return Thirst;
-        }
+        { return Thirst; }
         set
-        {
-            Thirst = value;
-        }
+        { Thirst = value; }
     }
 
     string State
@@ -121,11 +100,37 @@ public class Player : MonoBehaviour
         {
             for(int i = 0; i < AcceptableStates.Length; i++ )
             {
-                if (value == AcceptableStates[i])
+                if (value == AcceptableStates[i] && (State != "unconcious" || State != "dead") && value != "revival")
                 {
+                    //Value is anything that is not revival
                     State = value;
+                    return;
+                }else if((State == "unconcious" || State == "dead") && value == "revival")
+                {
+                    //Value is revival and player is either dead or unconcious
+                    State = value;
+                    return;
                 }
+                //Either an invalid state was given or player is concious but revival was used
             }
         }
     }
+    #endregion
+
+    void Update()
+    {
+        if (CurrentHP <= 0 && (State != "unconcious" || State != "dead"))
+        {
+            //Current hp is 0 or lower and player is not already dead/unconcious
+            player.State = "unconcious";
+        } else if (CurrentHP <= -MaxHP && State != "dead")
+        {
+            //Current hp is at the death threshold and player is not already dead
+            player.State = "dead";
+        }
+
+    }
+
+    
+
 }
